c452e54d22e4f275d05e719a2122bd8fb240b1ee
Leomar Linhares <leomar_linhares@hotmail.com>
2021-10-20 10:28:36 -0300

    adiciona o currenciesReducer ao rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import currenciesReducer from './currenciesReducer';
 // import wallet from './wallet';
 
 const rootReducer = combineReducers({
   user,
+  currenciesReducer,
 });
 
 export default rootReducer;

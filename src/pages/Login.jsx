6aaae737a68803b9921ec2f206541707a515fa73
Leomar Linhares <leomar_linhares@hotmail.com>
2021-10-20 12:53:49 -0300

    renomea arquivo de action do login e aplica a page Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { loginAction } from '../actions';
+import { loginAction } from '../actions/loginActions';
 
 class Login extends React.Component {
   constructor() {
